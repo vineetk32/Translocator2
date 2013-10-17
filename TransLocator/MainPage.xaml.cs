8b6d4de472fafe0484dd0e1402f318bfe9d99dda
Vineet <vineetk32@gmail.com>
2013-10-17 16:00:54 -0400

    Removed dead code and moved helper functions to Util.cs

--- TransLocator/MainPage.xaml.cs
+++ TransLocator/MainPage.xaml.cs
@@ -162,103 +162,20 @@ namespace Translocator
             isMapUptoDate = false;
         }
 
-        private LocationCollection DecodePolylinePoints(string encodedPoints)
-        {
-            if (encodedPoints == null || encodedPoints == "") return null;
-            LocationCollection poly = new LocationCollection();
-            char[] polylinechars = encodedPoints.ToCharArray();
-            int index = 0;
-
-            int currentLat = 0;
-            int currentLng = 0;
-            int next5bits;
-            int sum;
-            int shifter;
-
-            try
-            {
-                while (index < polylinechars.Length)
-                {
-                    // calculate next latitude
-                    sum = 0;
-                    shifter = 0;
-                    do
-                    {
-                        next5bits = (int)polylinechars[index++] - 63;
-                        sum |= (next5bits & 31) << shifter;
-                        shifter += 5;
-                    } while (next5bits >= 32 && index < polylinechars.Length);
-
-                    if (index >= polylinechars.Length)
-                        break;
-
-                    currentLat += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
-
-                    //calculate next longitude
-                    sum = 0;
-                    shifter = 0;
-                    do
-                    {
-                        next5bits = (int)polylinechars[index++] - 63;
-                        sum |= (next5bits & 31) << shifter;
-                        shifter += 5;
-                    } while (next5bits >= 32 && index < polylinechars.Length);
-
-                    if (index >= polylinechars.Length && next5bits >= 32)
-                        break;
-
-                    currentLng += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
-                    GeoCoordinate p = new GeoCoordinate();
-                    p.Latitude = Convert.ToDouble(currentLat) / 100000.0;
-                    p.Longitude = Convert.ToDouble(currentLng) / 100000.0;
-
-                    poly.Add(p);
-                }
-            }
-            catch (Exception ex)
-            {
-                // log it
-            }
-            return poly;
-        }
 
 
         private void addSegmentToMap(string segment,string routeColor)
         {
-            LocationCollection coordinates = DecodePolylinePoints(segment);
+            LocationCollection coordinates = Util.DecodePolylinePoints(segment);
             MapPolyline myPoly = new MapPolyline();
 
-            myPoly.Stroke = stringtoBrush(routeColor);
+            myPoly.Stroke = Util.stringtoBrush(routeColor);
             myPoly.StrokeThickness = 5;
             myPoly.Opacity = 1;
 
             myPoly.Locations = coordinates;
             myMap.Children.Add(myPoly);
         }
-
-        //private void addStopToMap(Stop currStop)
-        //{
-        //    MapLayer stopCirle = new MapLayer();
-
-        //    Pushpin pin = new Pushpin() { Location = new GeoCoordinate(currStop.location.lat, currStop.location.lng) };
-        //    pin.Width = 50;
-        //    pin.Height = 50;
-        //    pin.Template = (ControlTemplate)App.Current.Resources["StopPin"];
-        //    myMap.Children.Add(pin);
-        //}
-
-        private SolidColorBrush stringtoBrush(string colour)
-        {
-            return new SolidColorBrush(
-            Color.FromArgb(
-                Convert.ToByte("FF", 16),
-                Convert.ToByte(colour.Substring(0, 2), 16),
-                Convert.ToByte(colour.Substring(2, 2), 16),
-                Convert.ToByte(colour.Substring(4, 2), 16)
-            )
-            );
-        }
-        
 
         private void ParentPivot_LoadedPivotItem(object sender, PivotItemEventArgs e)
         {
@@ -278,14 +195,6 @@ namespace Translocator
                         }
                     }
                     isMapUptoDate = true;
-
-                    //foreach (Stop currStop in App.ViewModel.stopCache.Values)
-                    //{
-                    //    if (currStop.routes.Intersect(App.ViewModel.selectedRoutes).ToList().Count > 0)
-                    //    {
-                    //        addStopToMap(currStop);
-                    //    }
-                    //}
                 }
             }
 
--- TransLocator/Util.cs
+++ TransLocator/Util.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Net;
 using System.Windows;
+using Microsoft.Phone.Controls.Maps;
+using System.Windows.Media;
 
 namespace Translocator
 {
@@ -32,6 +34,80 @@ namespace Translocator
             StreamReader streamReader = new StreamReader(response.GetResponseStream());
             return streamReader.ReadToEnd();
         }
+
+        public static SolidColorBrush stringtoBrush(string colour)
+        {
+            return new SolidColorBrush(
+            Color.FromArgb(
+                Convert.ToByte("FF", 16),
+                Convert.ToByte(colour.Substring(0, 2), 16),
+                Convert.ToByte(colour.Substring(2, 2), 16),
+                Convert.ToByte(colour.Substring(4, 2), 16)
+            )
+            );
+        }
+
+
+        public static LocationCollection DecodePolylinePoints(string encodedPoints)
+        {
+            if (encodedPoints == null || encodedPoints == "") return null;
+            LocationCollection poly = new LocationCollection();
+            char[] polylinechars = encodedPoints.ToCharArray();
+            int index = 0;
+
+            int currentLat = 0;
+            int currentLng = 0;
+            int next5bits;
+            int sum;
+            int shifter;
+
+            try
+            {
+                while (index < polylinechars.Length)
+                {
+                    // calculate next latitude
+                    sum = 0;
+                    shifter = 0;
+                    do
+                    {
+                        next5bits = (int)polylinechars[index++] - 63;
+                        sum |= (next5bits & 31) << shifter;
+                        shifter += 5;
+                    } while (next5bits >= 32 && index < polylinechars.Length);
+
+                    if (index >= polylinechars.Length)
+                        break;
+
+                    currentLat += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+
+                    //calculate next longitude
+                    sum = 0;
+                    shifter = 0;
+                    do
+                    {
+                        next5bits = (int)polylinechars[index++] - 63;
+                        sum |= (next5bits & 31) << shifter;
+                        shifter += 5;
+                    } while (next5bits >= 32 && index < polylinechars.Length);
+
+                    if (index >= polylinechars.Length && next5bits >= 32)
+                        break;
+
+                    currentLng += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+                    GeoCoordinate p = new GeoCoordinate();
+                    p.Latitude = Convert.ToDouble(currentLat) / 100000.0;
+                    p.Longitude = Convert.ToDouble(currentLng) / 100000.0;
+
+                    poly.Add(p);
+                }
+            }
+            catch (Exception ex)
+            {
+                // log it
+            }
+            return poly;
+        }
+
 
     }
 }
